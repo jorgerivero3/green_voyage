55eeaa309c0dd672e2a301c86187a7462cb2df89
jorgerivero3 <20407259+jorgerivero3@users.noreply.github.com>
2025-01-29 01:23:06 -0600

    Added tests

--- app/main.py
+++ app/main.py
@@ -1,14 +1,29 @@
 from fastapi import FastAPI, Query
 from app.routes import route_lookup
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 app = FastAPI()
+
+origins = [
+  'http://localhost:3000'
+]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"]
+)
 
 #Includes the router
 app.include_router(route_lookup.router, prefix="/route", tags=["Route lookup"])
 
-# @app.get("/")
-# def read_root():
-#   return {"Hello": "World"}
+@app.get("/")
+def read_root():
+  return {"message": "Hello from FastAPI"}
 
 # @app.get("/items/{item_id}")
 # def read_item(item_id: int, q: Union[str, None] = None):
